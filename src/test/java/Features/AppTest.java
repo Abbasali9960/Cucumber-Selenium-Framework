148ebdc9bc90fa46eb67d521b208d1589d5dc1d5
Abbas H <abbasali9960@gmail.com>
2022-07-14 20:01:51 +0530

    updates test cases

--- src/test/java/Features/AppTest.java
+++ src/test/java/Features/AppTest.java
@@ -7,6 +7,17 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+//mvn -Dhttps.protocols=TLSv1.2 install
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/java/Features",
+		glue= {"stepDefinition"},
+	    plugin = {"pretty","html:target/cucumber-report/cucumber.html", "json:target/cucumber.json"},
+		monochrome = true
+		)
 public class AppTest 
 {
     /**
